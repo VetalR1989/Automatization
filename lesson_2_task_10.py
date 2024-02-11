1071ac7f70f67baedcf690646964bb6007123f41
Виталий Ребковец <142056850+VetalR1989@users.noreply.github.com>
2024-02-11 18:28:34 +0300

    Delete lesson_2_task_10.py

--- lesson_2_task_10.py
+++
@@ -1,9 +0,0 @@
-def bank(summa, year):
-    total = summa
-    for i in range(year):
-        total = total + (total * 0.10)
-    return total
-summa = int(input("Введите сумму: "))
-year = int(input("Введите количество лет: "))
-
-print(bank(summa, year))
