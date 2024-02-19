800c9cb983898a90fb8df64c7a27c41e0bc8323b
Виталий Ребковец <142056850+VetalR1989@users.noreply.github.com>
2024-02-19 23:10:00 +0300

    Add files via upload

---
+++ Task_2/lesson_2_task_5.py
@@ -0,0 +1,17 @@
+# Напишите функцию month_to_season(), которая принимает 1 аргумент — номер месяца — и возвращает название сезона, к которому относится этот месяц.
+# Например, передаем 2, на выходе получаем «Зима».
+
+def month_to_season(num):
+    num = int(num)
+    if num >= 9 and num <= 11:
+        print('Осень')
+    elif num >= 3 and num <= 5:
+        print('Весна')
+    elif num >= 6 and num <= 8:
+        print('Лето')
+    elif num in [1, 2, 12]:
+        print('Зима')
+    else:
+        print('Выберите номер месяца от 1 до 12')
+
+month_to_season(13)
